0bd6b5a59711a7aec0eaa8d0c5976a018b009436
TobiNguyen <nat21102002@gmail.com>
2023-02-12 18:09:14 +0700

    update file server.js

--- Backend_0/src/server.js
+++ Backend_0/src/server.js
@@ -1,19 +1,37 @@
+// Load environment variables from a .env file into process.env
 require('dotenv').config();
+
+// Require the express module
 const express = require('express');
+
+// Require the path module
 const path = require('path')
+
+// Require the custom module that configures the view engine
 const configViewEngine = require('./config/viewEngine')
+
+// Require the custom module that exports the web application routes
 const webRoutes = require('./routes/web')
 
+// Create an Express application instance
 const app = express();
+
+// Set the port number for the application
+// Use the value of the PORT environment variable if it's set,
+// or use 8888 as a default value if it's not
 const port = process.env.PORT || 8888;
+
+// Get the hostname from the HOST_NAME environment variable
 const hostname = process.env.HOST_NAME;
 
-//config template engine
+// Configure the view engine for the Express application
 configViewEngine(app)
 
-//route
+// Mount the webRoutes module at the /test path
 app.use('/test', webRoutes)
 
+// Start the Express application and listen on the specified port and hostname
 app.listen(port, hostname, () => {
+    // Log a message to the console indicating that the application is listening
     console.log(`Example app listening on port ${port}`)
 })
