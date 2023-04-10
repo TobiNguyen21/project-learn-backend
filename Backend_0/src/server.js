cb5498b2e13e9738aba58ebc94232defc41513ca
TobiNguyen <nat21102002@gmail.com>
2023-04-10 23:05:39 +0700

    update

--- Backend_0/src/server.js
+++ Backend_0/src/server.js
@@ -1,30 +1,16 @@
-// Load environment variables from a .env file into process.env
-require('dotenv').config();
-
-// Require the express module
-const express = require('express');
-
-// Require the path module
-const path = require('path')
-
-// Require the custom module that configures the view engine
-const configViewEngine = require('./config/viewEngine')
-
-// Require the custom module that exports the web application routes
-const webRoutes = require('./routes/web')
-
+require('dotenv').config();// Load environment variables from a .env file into process.env
+const express = require('express');// Require the express module
+const path = require('path')// Require the path module
+const configViewEngine = require('./config/viewEngine')// Require the custom module that configures the view engine
+const webRoutes = require('./routes/web')// Require the custom module that exports the web application routes
 const connection = require('./config/database');
-
-// Create an Express application instance
-const app = express();
+const app = express();// Create an Express application instance
 
 // Set the port number for the application
 // Use the value of the PORT environment variable if it's set,
 // or use 8888 as a default value if it's not
 const port = process.env.PORT || 8888;
-
-// Get the hostname from the HOST_NAME environment variable
-const hostname = process.env.HOST_NAME;
+const hostname = process.env.HOST_NAME;// Get the hostname from the HOST_NAME environment variable
 
 //config template engine
 //Purpose: to enable rendering of dynamic content & data to be sent to the client-side
@@ -36,12 +22,6 @@ configViewEngine(app)
 
 // Mount the webRoutes module at the / path
 app.use('/', webRoutes)
-
-//test connection
-
-
-// simple query
-
 
 // Start the Express application and listen on the specified port and hostname
 app.listen(port, hostname, () => {
