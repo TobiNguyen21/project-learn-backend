208abe62f958d6d728917d977a8ae1a9ad4eb5cb
TobiNguyen <nat21102002@gmail.com>
2023-04-24 22:00:55 +0700

    Read/Write mongodb driver

--- Backend_0/src/server.js
+++ Backend_0/src/server.js
@@ -6,6 +6,8 @@ const webRoutes = require('./routes/web');
 const apiRoutes = require('./routes/api');
 const fileUpload = require('express-fileupload');
 const connection = require('./config/database');
+const { MongoClient } = require("mongodb");
+const { collection } = require('./models/customer');
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -27,7 +29,25 @@ app.use('/v1/api/', apiRoutes);
 // using seft function
 (async () => {
     try {
-        await connection();
+        //using mogoose
+        // await connection();
+
+        //using mongodb driver
+        const url = process.env.DB_HOST_WITH_DRIVER;
+        const client = new MongoClient(url);
+
+        await client.connect();
+        console.log('Connected successfully to server');
+
+        const dbName = process.env.DB_NAME;
+        const db = client.db(dbName);
+        const coll = db.collection('customers')
+
+        // await coll.insertOne({ name: 'Jackie Robinson 2', address: 'Ho Chi Minh' });
+
+        console.log(">> find = ", await coll.findOne({ address: "Ho Chi Minh" }));
+
+        // Database Name
         app.listen(port, hostname, () => {
             // Log a message to the console indicating that the application is listening
             console.log(`Backend zero app listening on port ${port}`)
